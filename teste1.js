ed7e4a6d453a1cee9cfd2ff94e382a0577d78d03
robinndev <robson_dev@hotmail.com>
2021-03-31 21:21:34 -0300

    Commit final

---
+++ teste1.js
@@ -0,0 +1,37 @@
+let num1 = 11; 
+let num2 = 10; 
+let num3 = 15;
+
+//soma 
+
+let soma = num1 + num2; 
+
+//subtração 
+
+let subtração = num1 - num2;
+
+//divisão 
+
+let divisão = num1 / num2;
+
+//multiplicação 
+
+let multiplicação = num1 * num2; 
+
+//comparação 
+
+let comparação = num1 === num2;
+
+//condicional 
+
+if(num1 > num2) {
+    console.log("num1 wins"); 
+} else {
+    console.log("num2 wins")
+}
+
+// como saber se o num1 + num2 é par?
+
+let isEven = ((num1 + num2) %2) == 0 
+
+console.log(isEven);
