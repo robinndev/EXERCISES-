cbd49ff610ccc4266e19ff11f3f4b85b10804ff9
Robinndev <robson_dev@hotmail.com>
2021-04-14 18:51:29 -0300

    Gerador de números de forma aléatoria

---
+++ megasena.js
@@ -0,0 +1,31 @@
+let megaSenaNumbers = []
+
+// let randomNumber = Math.random() * 60
+
+// console.log(randomNumber)
+
+// let randomNumberRedondo = Math.ceil(randomNumber)
+
+// console.log(randomNumberRedondo)
+
+let firstNumber = Math.random() * 60; 
+firstNumber= Math.ceil(firstNumber)
+
+let secondeNumber = Math.random() * 60;  
+secondeNumber= Math.ceil(secondeNumber)
+
+let threeNumber = Math.random() * 60; 
+threeNumber= Math.ceil(threeNumber) 
+
+let fourNumber = Math.random() * 60; 
+fourNumber= Math.ceil(fourNumber)
+
+let fiveNumber = Math.random() * 60;  
+fiveNumber= Math.ceil(fiveNumber)
+
+let sixNumber = Math.random() * 60;  
+sixNumber= Math.ceil(sixNumber)
+
+megaSenaNumbers = [firstNumber, secondeNumber,threeNumber,fourNumber,fiveNumber,sixNumber]
+
+console.log (megaSenaNumbers)
